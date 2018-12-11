4186a39e412108429a7ffecca9e73486a13ca355
peterforro <peterforro@gmail.com>
2018-12-11 14:20:16 +0100

    HTML templates & home/game routes

--- server.py
+++ server.py
@@ -1,1 +1,22 @@
+from flask import Flask, render_template, url_for, redirect, request
 
+app = Flask(__name__)
+
+
+
+@app.route('/')
+def index():
+    return render_template("index.html")
+
+
+
+@app.route('/game')
+def game():
+    return render_template("game.html")
+
+
+
+if __name__ == "__main__":
+    app.run(
+        debug=True
+    )
