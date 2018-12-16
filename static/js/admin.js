6433306d842c23b9851c34481720b30cd25ccac0
peterforro <peterforro@gmail.com>
2018-12-16 14:57:37 +0100

    select all btn for checkboxes, messy but prettier css design

--- static/js/admin.js
+++ static/js/admin.js
@@ -1,4 +1,4 @@
-function init(){
+function remove_flash_messages(){
     let flashes = document.querySelectorAll(".flashes");
     if(flashes.length){
       for (flash of flashes){
@@ -8,6 +8,31 @@ function init(){
           });
       }
     }
+}
+
+
+
+function selectAllBtn() {
+    let btn = document.querySelector("#selectAll");
+    if(btn !== null){
+        btn.addEventListener("click",function(){
+            let checkboxes = document.querySelectorAll("input[type=checkbox]");
+            for (checkbox of checkboxes){
+                checkbox.checked = true;
+            }
+        })
+    }
+}
+
+
+
+
+
+
+
+function init(){
+    remove_flash_messages();
+    selectAllBtn();
 }
 
 init();
